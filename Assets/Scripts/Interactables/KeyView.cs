6bf97c9892ed4406b2756b10feaf38bbf0a3b8d3
Chintan Patel <chintanpatelit024@gmail.com>
2025-06-02 15:19:20 +0530

    Added: Improving UI logic

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -9,6 +9,7 @@ public class KeyView : MonoBehaviour, IInteractable
         GameService.Instance.GetInstructionView().HideInstruction();
         GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
         currentKeys++;
+        EventService.Instance.OnKeyPickedUp.InvokeEvent(currentKeys);
 
         gameObject.SetActive(false);
     }
